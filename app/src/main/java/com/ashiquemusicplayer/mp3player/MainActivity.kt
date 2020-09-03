00dbd4a99c99877b2ecac06a938cdec1da749b6d
Ashique Bava <ashiquebava.ab@gmail.com>
2020-09-03 23:02:39 +0530

    Design changes and music playing programmes inserted

--- app/src/main/java/com/ashiquemusicplayer/mp3player/MainActivity.kt
+++ app/src/main/java/com/ashiquemusicplayer/mp3player/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.ashiquemusicplayer.mp3player
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mp: MediaPlayer
+    private var totalTime: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mp = MediaPlayer.create(this, R.raw.sample)
+        mp.isLooping = true
+
+        playPauseButton.setOnClickListener {
+            if (mp.isPlaying) {
+                mp.pause()
+                playPauseButton.setBackgroundResource(R.drawable.play)
+            } else {
+                mp.start()
+                playPauseButton.setBackgroundResource(R.drawable.pause)
+            }
+        }
     }
 }
