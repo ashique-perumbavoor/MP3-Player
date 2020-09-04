b6905d1c91fd6843330eef99acdd83dba4d7843c
Ashique Bava <ashiquebava.ab@gmail.com>
2020-09-04 23:15:02 +0530

    Seekerbar, elapsed time and remaining time added

--- app/src/main/java/com/ashiquemusicplayer/mp3player/MainActivity.kt
+++ app/src/main/java/com/ashiquemusicplayer/mp3player/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.ashiquemusicplayer.mp3player
 
+import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.view.View
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,21 +15,76 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mp: MediaPlayer
     private var totalTime: Int = 0
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mp = MediaPlayer.create(this, R.raw.sample)
+        mp = MediaPlayer.create(this, R.raw.closer)
         mp.isLooping = true
+        totalTime = mp.duration
 
-        playPauseButton.setOnClickListener {
-            if (mp.isPlaying) {
-                mp.pause()
-                playPauseButton.setBackgroundResource(R.drawable.play)
-            } else {
-                mp.start()
-                playPauseButton.setBackgroundResource(R.drawable.pause)
+
+        progressBar.max = totalTime
+        progressBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    if (fromUser) {
+                        mp.seekTo(progress)
+                    }
+                }
+                override fun onStartTrackingTouch(p0: SeekBar?) {
+                }
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+                }
             }
+        )
+
+        Thread(Runnable {
+            while (mp != null) {
+                try {
+                    var msg = Message()
+                    msg.what = mp.currentPosition
+                    handler.sendMessage(msg)
+                    Thread.sleep(1)
+                } catch (e: InterruptedException) {
+                }
+            }
+        }).start()
+    }
+
+    @SuppressLint("HandlerLeak")
+    var handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            var currentPosition = msg.what
+            progressBar.progress = currentPosition
+            var elapsedTime = createTimeLabel(currentPosition)
+            elapsedTimeLabel.text = elapsedTime
+            var remainingTime = createTimeLabel(totalTime - currentPosition)
+            remainingTimeLabel.text = "-$remainingTime"
+        }
+    }
+
+    fun createTimeLabel(time: Int): String {
+        var timeLabel = ""
+        var min = time / 1000 / 60
+        var sec = time / 1000 % 60
+
+        timeLabel = "$min:"
+        if (sec < 10) timeLabel += "0"
+        timeLabel += sec
+
+        return timeLabel
+    }
+
+    fun playBtnClick(v: View) {
+
+        if (mp.isPlaying) {
+            mp.pause()
+            playPauseButton.setBackgroundResource(R.drawable.play)
+        } else {
+            mp.start()
+            playPauseButton.setBackgroundResource(R.drawable.pause)
         }
     }
 }
