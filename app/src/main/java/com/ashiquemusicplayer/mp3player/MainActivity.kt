680f6d76725aff1048ee375a6e5124bd77098870
Ashique Bava <ashiquebava.ab@gmail.com>
2020-09-08 19:09:48 +0530

    Design Prototype added

--- app/src/main/java/com/ashiquemusicplayer/mp3player/MainActivity.kt
+++ app/src/main/java/com/ashiquemusicplayer/mp3player/MainActivity.kt
@@ -8,7 +8,12 @@ import android.os.Handler
 import android.os.Message
 import android.view.View
 import android.widget.SeekBar
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
